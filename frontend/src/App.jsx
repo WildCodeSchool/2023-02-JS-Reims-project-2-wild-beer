9888df22899dddc000cb8b80adc5754196517991
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-04-12 11:24:41 +0200

    Modification of App.jsx using Fetch to retrieve APIs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+
 import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState([]);
+
+  const beerFetch = () => {
+    fetch("http://localhost:5000/beers")
+      .then((response) => response.json())
+      .then((data) => {
+        setFetchedData(data);
+      });
+  };
+
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
+      <button type="button" onClick={beerFetch}>
+        Viens boire un coup !
+      </button>
+      {fetchedData.map((beer) => (
+        <p key={beer.id}>
+          {beer.name}
+          {beer.ebc}
+          {beer.srm}
+          {beer.ibu}
+          {beer.abv}
+        </p>
+      ))}
+      ;
     </div>
   );
 }
