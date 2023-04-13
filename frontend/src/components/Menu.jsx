5cf50a9532c9766d8f1eb716596f42c0cc831562
Rinawaticodes <kyurei.kim@gmail.com>
2023-04-13 16:32:09 +0200

    Merge all components and put state from every componets to navbar

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,27 +1,27 @@
-import { useState } from "react";
+import PropTypes from "prop-types";
 
-function Menu() {
-  const [isMenu, setIsMenu] = useState(true);
-  const handleChangeMenu = () => {
-    setIsMenu(!isMenu);
-    console.info(isMenu);
-  };
-
+function Menu({ handleChangeMenu }) {
   return (
-    <form className="firstMenu">
-      <label>
-        Quelle est votre pseudo ?
-        <input type="text" className="inputFirstMenu" />
-      </label>
-      <button
-        type="button"
-        onClick={handleChangeMenu}
-        className="buttonFirstMenu"
-      >
-        Jouer
-      </button>
-    </form>
+    <section className="containerMenu">
+      <form className="firstMenu">
+        <label>
+          Quelle est votre pseudo ?
+          <input type="text" className="inputFirstMenu" />
+        </label>
+        <button
+          type="button"
+          onClick={handleChangeMenu}
+          className="buttonFirstMenu"
+        >
+          Jouer
+        </button>
+      </form>
+    </section>
   );
 }
+
+Menu.propTypes = {
+  handleChangeMenu: PropTypes.func.isRequired,
+};
 
 export default Menu;
