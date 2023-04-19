1ac9de14830ab7b95a5622958ec45b822f41f464
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-04-19 10:53:38 +0200

    Addition of Routes/Routes with react-router-dom and readjustment of the App function related to Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,15 @@
-import { useState } from "react";
-
+import { Routes, Route } from "react-router-dom";
 import Menu from "./components/Menu";
-
 import "./App.css";
-
 import Navbar from "./components/Navbar";
 
-import Rules from "./components/Rules";
-
 function App() {
-  const [rules, setRules] = useState(false);
-  const [isMenu, setIsMenu] = useState(true);
-
-  const handleChangeMenu = () => {
-    setIsMenu(!isMenu);
-    if (rules && !isMenu) {
-      setRules(!rules);
-    }
-  };
-  const showMeTheRules = () => {
-    setRules(!rules);
-  };
-
   return (
     <div className="App">
-      {isMenu ? (
-        <Menu handleChangeMenu={handleChangeMenu} />
-      ) : (
-        <Navbar
-          handleChangeMenu={handleChangeMenu}
-          showMeTheRules={showMeTheRules}
-        />
-      )}
-      {rules && <Rules />}
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/wild-beer-gaming" element={<Navbar />} />
+      </Routes>
     </div>
   );
 }
