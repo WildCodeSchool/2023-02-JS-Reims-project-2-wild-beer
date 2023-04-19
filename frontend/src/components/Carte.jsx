72cc796026e5cc37ae2607d1b15a84668b2c1eae
Rinawaticodes <kyurei.kim@gmail.com>
2023-04-19 14:05:32 +0200

    this is commit for display some API using fetch & useeffect

--- frontend/src/components/Carte.jsx
+++ frontend/src/components/Carte.jsx
@@ -2,21 +2,29 @@ import React, { useState, useEffect } from "react";
 
 function Carte() {
   const [cardList, setCardList] = useState([]);
-
+  /* This is fetch API */
   useEffect(() => {
-    fetch("http://localhost:5000/beers")
+    fetch("https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6")
       .then((response) => response.json())
-      .then((data) => setCardList(data));
+      .then((data) => setCardList(data.slice(0, 5)));
   }, []);
 
   return (
-    <div>
-      <ul>
-        {cardList.map((card) => (
-          <li key={card.id}>{card.name}</li>
-        ))}
-        ;
-      </ul>
+    <div className="containertest">
+      {cardList.map((card) => (
+        <div className="container" key={card.id}>
+          <ul>
+            <li>{card.name}</li>
+            <li>{card.id}</li>
+            <li>{card.ibu}</li>
+            <li>{card.abv}</li>
+            <li>{card.ebc}</li>
+            <li>{card.srm}</li>
+            <li>{card.tagline}</li>
+          </ul>
+        </div>
+      ))}
+      ;
     </div>
   );
 }
