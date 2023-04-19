89955111dad8df6e8462858a0501aa73b4e35372
Carenjot <carenjot.aurelien@hotmail.fr>
2023-04-19 17:19:43 +0200

    Merge of the different PR of the sprint 2

--- frontend/src/components/Carte.jsx
+++ frontend/src/components/Carte.jsx
@@ -1,31 +1,25 @@
-import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-function Carte() {
-  const [cardList, setCardList] = useState([]);
-  /* This is fetch API */
-  useEffect(() => {
-    fetch("https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6")
-      .then((response) => response.json())
-      .then((data) => setCardList(data.slice(0, 5)));
-  }, []);
-
+function Carte({ handcard }) {
   return (
-    <div className="containertest">
-      {cardList.map((card) => (
-        <div className="container" key={card.id}>
-          <ul>
-            <li>{card.name}</li>
-            <li>{card.id}</li>
-            <li>{card.ibu}</li>
-            <li>{card.abv}</li>
-            <li>{card.ebc}</li>
-            <li>{card.srm}</li>
-            <li>{card.tagline}</li>
-          </ul>
-        </div>
-      ))}
-      ;
-    </div>
+    <section className="card" key={handcard.id}>
+      <h1>{handcard.name}</h1>
+      <ul>
+        <li>EBC : {handcard.ebc}</li>
+        <li>ABV : {handcard.abv}</li>
+        <li>IBU : {handcard.ibu}</li>
+        <li>SRM : {handcard.srm}</li>
+      </ul>
+    </section>
   );
 }
+
+Carte.defaultProps = {
+  handcard: {},
+};
+
+Carte.propTypes = {
+  handcard: PropTypes.objectOf(),
+};
+
 export default Carte;
--- frontend/src/components/board-components/Hand.jsx
+++ frontend/src/components/board-components/Hand.jsx
@@ -1,5 +1,25 @@
+import React, { useState, useEffect } from "react";
+import Carte from "../Carte";
+
 function Hand() {
-  return <h1>hello from Hand</h1>;
+  const [cardList, setCardList] = useState([]);
+  /* This is fetch API */
+  useEffect(() => {
+    fetch("https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6")
+      .then((response) => response.json())
+      .then((data) => {
+        setCardList(data.slice(0, 5));
+        console.info(data.slice(0, 5));
+      });
+  }, []);
+  return (
+    <div className="containertest">
+      {cardList.map((card) => (
+        <Carte handcard={card} key={card.id} />
+      ))}
+      ;
+    </div>
+  );
 }
 
 export default Hand;
