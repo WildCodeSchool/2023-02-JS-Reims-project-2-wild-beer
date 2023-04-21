46eb8a26039a0b7e9732e9454d033608dfe45efd
Rinawaticodes <kyurei.kim@gmail.com>
2023-04-21 10:09:12 +0200

    change background homepage

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,21 +1,28 @@
 import { Link } from "react-router-dom";
+import Bar from "../Images/wild-beer-logo.png";
 
 function Menu() {
   return (
-    <section className="containerMenu">
-      <form className="firstMenu">
-        <label>
-          Quelle est votre pseudo ?
-          <input type="text" className="inputFirstMenu" />
-        </label>
+    <div className="homepage">
+      <div id="showgame">
+        <img src={Bar} alt="" className="logobar" />
+        <p className="show">DECOUVRE LA ZYTHOLOGIE EN T'AMUSANT</p>
+      </div>
+      <section className="containerMenu">
+        <form className="firstMenu">
+          <label>
+            Quelle est votre pseudo ?
+            <input type="text" className="inputFirstMenu" />
+          </label>
 
-        <Link to="/wild-beer-gaming">
-          <button type="button" className="buttonFirstMenu">
-            Jouer
-          </button>
-        </Link>
-      </form>
-    </section>
+          <Link to="/wild-beer-gaming">
+            <button type="button" className="buttonFirstMenu">
+              Jouer
+            </button>
+          </Link>
+        </form>
+      </section>
+    </div>
   );
 }
 
