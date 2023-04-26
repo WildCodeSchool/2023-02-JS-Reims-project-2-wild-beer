e710640b50e2d4b5df4602857694c530b6d64346
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-04-26 16:00:18 +0200

    Recovering the input to insert it in the Board.jsx component

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Menu() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
   return (
     <section className="containerMenu">
       <form className="firstMenu">
         <label>
           Quelle est votre pseudo ?
-          <input type="text" className="inputFirstMenu" />
+          <input
+            type="text"
+            className="inputFirstMenu"
+            value={inputValue}
+            onChange={handleChange}
+          />
         </label>
 
-        <Link to="/wild-beer-gaming">
+        <Link to={`/wild-beer-gaming/${inputValue}`}>
           <button type="button" className="buttonFirstMenu">
             Jouer
           </button>
