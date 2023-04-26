9e7f24b3215a319bd7eef0422b701b5a8cc8f902
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-04-26 15:59:05 +0200

    Adding the useParams and the name-player div

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,13 +1,18 @@
+import { useParams } from "react-router-dom";
 import Navbar from "./board-components/Navbar";
 import Gamefield from "./board-components/Gamefield";
 
 import "./board-components/Board.css";
 
 function Board() {
+  const userName = useParams();
   return (
     <div className="main-board">
       <Navbar />
       <Gamefield />
+      <div className="div-name-player">
+        <h1 className="name-player">{userName.id}</h1>
+      </div>
     </div>
   );
 }
