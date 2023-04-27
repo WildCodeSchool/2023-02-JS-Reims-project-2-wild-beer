f12e27c031ee76c3ff46341788e37d58841e64e2
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-04-27 16:13:38 +0200

    Changed h1 tag to p tag and added braces to useParams

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,17 +1,16 @@
 import { useParams } from "react-router-dom";
 import Navbar from "./board-components/Navbar";
 import Gamefield from "./board-components/Gamefield";
-
 import "./board-components/Board.css";
 
 function Board() {
-  const userName = useParams();
+  const { userName } = useParams();
   return (
     <div className="main-board">
       <Navbar />
       <Gamefield />
       <div className="div-name-player">
-        <h2 className="name-player">{userName.id}</h2>
+        <p className="name-player">{userName.username}</p>
       </div>
     </div>
   );
