1ba20f5d4ef91484dc2411fe845b787bf9cda9f2
Carenjot <carenjot.aurelien@hotmail.fr>
2023-04-27 15:44:53 +0200

    add default value for ebc, ibu, abv and srm. Add a limitation for the name of the beer

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -7,15 +7,35 @@ function Card({
   putCardOnField,
   isPlayed,
 }) {
+  // Check si les valeurs sont null (cas rare mais possible)
+
+  const ebc = card.ebc === null ? Math.floor(Math.random() * 100) : card.ebc;
+  const srm = card.srm === null ? Math.floor(ebc * 0.508) : card.srm;
+  const abv = card.abv === null ? (Math.random() * 15).toFixed(2) : card.abv;
+  const ibu = card.ibu === null ? Math.floor(Math.random() * 100) : card.ibu;
+
+  // Eviter que le nom de la bière soit trop long
+  const maxCharName = 15;
+  let beerName = card.name;
+  beerName = beerName.split(" ");
+  let totalChar = 0;
+  let finalName = "";
+  for (let i = 0; i < beerName.length; i += 1) {
+    if (totalChar < maxCharName) {
+      totalChar += beerName[i].length;
+      finalName += `${beerName[i]} `;
+    }
+  }
+
   const getColorClassName = () => {
     let colorClassName = "";
-    if (card.ebc <= 20) {
+    if (ebc <= 15) {
       colorClassName = "blonde";
-    } else if (card.ebc <= 30) {
+    } else if (ebc <= 30) {
       colorClassName = "ambre";
-    } else if (card.ebc <= 75) {
+    } else if (ebc <= 75) {
       colorClassName = "brunes";
-    } else if (card.ebc > 75) {
+    } else if (ebc > 75) {
       colorClassName = "noires";
     }
     return colorClassName;
@@ -30,12 +50,12 @@ function Card({
           changeCardSelect(card.id);
         }}
       >
-        <h1>{card.name}</h1>
+        <h1>{finalName}</h1>
         <ul>
-          <li>EBC : {card.ebc}</li>
-          <li>ABV : {card.abv}</li>
-          <li>IBU : {card.ibu}</li>
-          <li>SRM : {card.srm}</li>
+          <li>EBC : {ebc}</li>
+          <li>ABV : {abv}</li>
+          <li>IBU : {ibu}</li>
+          <li>SRM : {srm}</li>
         </ul>
       </button>
       {cardSelect === card.id && isPlayed && (
@@ -57,10 +77,10 @@ Card.propTypes = {
   card: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
-    ebc: PropTypes.number.isRequired,
-    abv: PropTypes.number.isRequired,
-    ibu: PropTypes.number.isRequired,
-    srm: PropTypes.number.isRequired,
+    ebc: PropTypes.number,
+    abv: PropTypes.number,
+    ibu: PropTypes.number,
+    srm: PropTypes.number,
   }).isRequired,
   cardSelect: PropTypes.number.isRequired,
   changeCardSelect: PropTypes.func.isRequired,
