66fbc4b7cf5aa257ec48e07e7ffa4d7b5e7fc555
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-04-27 16:14:28 +0200

    Adding braces to useParams

--- frontend/src/components/Score.jsx
+++ frontend/src/components/Score.jsx
@@ -4,12 +4,12 @@ import { useParams } from "react-router-dom";
 function Score() {
   const [onePlayerScore, setOnePlayerScore] = useState(0);
   const [twoPlayerScore, setTwoPlayerScore] = useState(0);
-  const userName = useParams();
+  const { userName } = useParams();
 
   return (
     <div className="scoreBoard">
       <div className="scoreBoard-name">
-        {userName.id}
+        {userName.username}
         <div>{onePlayerScore}</div>
         <button
           type="button"
