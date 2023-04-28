7949f8aa17d92cf4cba6178d05b95f378a6052f4
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-04-28 09:08:51 +0200

    Adding Link routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-// import Menu from "./components/Menu";
+import Menu from "./components/Menu";
 import "./App.css";
 import Board from "./components/Board";
 import AgeVerification from "./components/AgeVerification";
-import Menu from "./components/Menu";
 import Mineur from "./components/Mineur";
 
 function App() {
@@ -12,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AgeVerification />} />
         <Route path="/Menu" element={<Menu />} />
+        <Route path="/Menu/wild-beer-gaming/" element={<Board />} />
         <Route path="/Menu/wild-beer-gaming/:username" element={<Board />} />
         <Route path="/Mineur" element={<Mineur />} />
       </Routes>
