5689a0b2b0a6b5a568368125ffe7e6cd3041d7d0
Rinawaticodes <kyurei.kim@gmail.com>
2023-04-28 10:19:12 +0200

    add peanut img for rules list

--- frontend/src/components/Rules.jsx
+++ frontend/src/components/Rules.jsx
@@ -1,34 +1,45 @@
+import Peanut from "../Images/peanutfull.png";
+
 function Rules() {
   return (
     <section className="listRules">
       <h1 className="titleRules">Règles du jeu</h1>
       <ul className="rules">
-        <li>Un joueur se bat contre un ordinateur.</li>
+        <li>
+          <img src={Peanut} alt="peanut" />
+          Un joueur se bat contre un ordinateur.
+        </li>
 
         <li>
+          <img src={Peanut} alt="peanut" />
           Le joueur et l'ordinateur commencent avec 5 cartes bière chacun
           (distribuées aléatoirement).
         </li>
 
         <li>
+          <img src={Peanut} alt="peanut" />
           Chaque carte possédera des attributs bière correspondant aux
           différents thèmes des rounds.
         </li>
 
         <li>
+          <img src={Peanut} alt="peanut" />
           La partie se déroule en 5 rounds, chaque round aura un thème défini.
         </li>
 
         <li>
+          <img src={Peanut} alt="peanut" />
           Après la sélection de la carte par les joueurs, le meilleur score
           correspondant au thème du round remporte un point apéro.
         </li>
 
         <li>
+          <img src={Peanut} alt="peanut" />
           Celui qui possède le plus de point apéro en fin de partie gagne.
         </li>
 
         <li>
+          <img src={Peanut} alt="peanut" />
           Description caractéristiques: IBU = amertume, ABV = degré d'alcool,
           EBC = indice de couleur, SRM = transparence.
         </li>
