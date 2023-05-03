2e56b052dd18b7c2e2e19234d691df8abf9624c5
JulienBenoit7 <julien-benoit@hotmail.fr>
2023-05-03 13:08:41 +0200

    round + css

--- frontend/src/components/board-components/Round.jsx
+++ frontend/src/components/board-components/Round.jsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 
 function Round() {
+  const shuffled = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+  };
+
   const rounds = ["SRM", "IBU", "ABV", "EBC"];
-  const [value, setValue] = useState();
+  const shuffledRounds = shuffled([...rounds]);
+  const visitedRounds = [];
+  const [value, setValue] = useState(rounds[0]);
 
   const changeRound = (arrRounds) => {
     const round = Math.round(Math.random() * (arrRounds.length - 1));
     setValue(arrRounds[round]);
   };
+
+  let round;
+  do {
+    round = shuffledRounds.shift();
+    if (!visitedRounds.includes(round)) {
+      visitedRounds.push(round);
+      break;
+    }
+  } while (shuffledRounds.length > 0);
 
   return (
     <>
